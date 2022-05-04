699882ea3035fe397253633f2718318d0231fef8
Leandro Regis <leandro.saraiva@prosperi.com.br>
2022-05-04 16:42:54 -0300

    Added Model and Mock Repository

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Data;
+using CommandAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Controllers
@@ -10,7 +12,28 @@ namespace CommandAPI.Controllers
   [ApiController]
   public class CommandsController : ControllerBase
   {
+    private readonly ICommandAPIRepo _repository;
+    public CommandsController(ICommandAPIRepo repository)
+    {
+      _repository = repository;
+    }
+
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get() => new string[] { "This", "is", "hard", "coded" };
+    public ActionResult<IEnumerable<Command>> GetAllCommands()
+    {
+      var commandItems = _repository.GetAllCommands();
+      return Ok(commandItems);
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<Command> GetCommandById(int id)
+    {
+      var commandItem = _repository.GetCommandById(id);
+      if (commandItem == null)
+      {
+        return NotFound();
+      }
+      return Ok(commandItem);
+    }
   }
 }
--- src/CommandAPI/Models/Command.cs
+++ src/CommandAPI/Models/Command.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace CommandAPI.Models
@@ -17,6 +13,6 @@ namespace CommandAPI.Models
     [Required]
     public string Platform { get; set; }
     [Required]
-    public string CommadLine { get; set; }
+    public string CommandLine { get; set; }
   }
 }
